2bcb8f2a16f75a898dc80113ebf41130d064770c
bunmigrey <bunmigrey@icloud.com>
2023-04-12 22:30:01 +0100

    starting with AI API

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-require('dotenv').config({ path: './key.env' });
-require('dotenv').config({ path: './lol.env' });
+require('dotenv').config({ path: './keys.env' });
+const { Configuration, OpenAIApi } = require("openai");
 
 
 
-const port = process.env.DB_HOST;
-const key = process.env.MAIN_KEY;
-const local = process.env.LOCAL;
-console.log(port);
-console.log(local)
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listModels();
+(async function (){
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test"
+    })
+
+    console.log(response);
+})()
+
+
